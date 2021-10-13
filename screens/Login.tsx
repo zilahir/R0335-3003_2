dd5f598171a087eade55ca59379a52f9a38d7133
Richard Zilahi <richard.zilahi@varjo.com>
2021-10-14 01:44:12 +0300

    implementing login screen

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,24 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
+import React, { useState} from 'react'
 import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/WelcomeScreen';
 import { Text, View } from '../components/Themed';
+import { TextInput } from 'react-native-gesture-handler';
+import { themeColors } from '../utils/consts';
 
 export default function ModalScreen() {
+
+  const [userName, setUserName] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Login</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo />
-
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
+      <View>
+        <TextInput
+          style={[styles.textInput]}
+          onChangeText={(value) => setUserName(value)}
+        />
+        <TextInput
+          style={[styles.textInput]}
+          onChangeText={(value) => setUserName(value)}
+        />
+      </View>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    width: 300,
+    marginVertical: 10,
+    backgroundColor: themeColors.textInputBgColor,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
--- utils/consts.ts
+++ utils/consts.ts
@@ -2,5 +2,6 @@ import hexToRgba from 'hex-to-rgba';
 
 export const themeColors = {
     extraFeature: hexToRgba('ff6961', 1),
-    minimumFeature: hexToRgba('fdfd96', 1)
+    minimumFeature: hexToRgba('fdfd96', 1),
+    textInputBgColor: hexToRgba('f1f1f1', 1)
 }
