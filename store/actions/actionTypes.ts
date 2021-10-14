23ebdb8c98cadc83b0c2fcedd40c67429186ca54
Richard Zilahi <richard.zilahi@varjo.com>
2021-10-14 18:39:29 +0300

    implementing action(reducers for creating users

--- store/actions/actionTypes.ts
+++ store/actions/actionTypes.ts
@@ -1,1 +1,2 @@
 export const TEST = 'TEST'
+export const CREATE_NEW_USER = 'CREATE_NEW_USER'
---
+++ store/actions/createNewUser.ts
@@ -0,0 +1,14 @@
+import * as actionTypes from './actionTypes'
+
+export interface User {
+    userId: string,
+    userName: string,
+    password: string
+}
+
+export const createNewUser = (newUser: Omit<User, 'userId'>) => ({
+    type: actionTypes.CREATE_NEW_USER,
+    payload: {
+        newUser,
+    }
+})
